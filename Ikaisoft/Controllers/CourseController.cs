a92e88800808144a2a3a476e489b6c4890af1f9a
MYPC <shubhruti5@gmail.com>
2025-08-29 16:03:36 +0530

    HTML Compiler Added

---
+++ Ikaisoft/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ikaisoft.Controllers
+{
+    public class CourseController : Controller
+    {
+       
+        public IActionResult HTML()
+        {
+            return View();
+        }
+        public IActionResult Lesson(int id)
+        {
+            if (id < 1) id = 1; 
+            return PartialView($"~/Views/HTML/_lesson{id}.cshtml");
+        }
+        public IActionResult CSS()
+        {
+            return View();
+        }
+        public IActionResult CssLesson(int id)
+        {
+            if (id < 1) id = 1;
+            return PartialView($"~/Views/HTML/_lesson{id}.cshtml");
+        }
+    }
+}
